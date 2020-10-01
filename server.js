6de196b556a91a2f96ee2dd38c2a84a8819517a6
Supreeth <supreethub96@gmail.com>
2020-09-30 23:14:19 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,32 +1,18 @@
 const express = require('express');
+const movedData = require('./movedData.json');
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Rent',
-        budget: 380
-    },
-    {
-        title: 'Grocery',
-        budget: 90
-    },
-    {
-        title: 'Netflix',
-        budget: 25
-    }
-    ]
-};
+
 
 app.get('/hello', (req,res) => {
     res.send ('Hello World!');
 });
 
 app.get('/budget', (req,res) => {
-    res.json(budget);
+    res.json(movedData)
 });
 
 app.listen(port, () => {
